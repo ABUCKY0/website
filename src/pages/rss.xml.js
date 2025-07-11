f95902cdbc7d4ad260e8fb9e99e094cfd5c53327
ABUCKY0 <81783950+ABUCKY0@users.noreply.github.com>
2025-07-11 02:56:12 -0500

    fix: update blog title, description, and remove /content/ from url

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,13 +5,17 @@ export async function GET(context) {
   const postImportResult = import.meta.glob('./posts/content/*.md', { eager: true });
   const posts = Object.values(postImportResult);
   return rss({
-    title: 'Buzz’s Blog',
-    description: 'A humble Astronaut’s guide to the stars',
+    title: "Aaron's Blog",
+    description: 'A collection of my thoughts, projects, and experiences.',
     site: context.site,
-    items: await Promise.all(posts.map(async (post) => ({
-      link: post.url,
-      content: sanitizeHtml((await post.compiledContent())),
-      ...post.frontmatter,
-    }))),
+    items: await Promise.all(posts.map(async (post) => {
+      // Remove '/content' from the post URL if present
+      let link = post.url.replace('/content', '');
+      return {
+        link,
+        content: sanitizeHtml((await post.compiledContent())),
+        ...post.frontmatter,
+      };
+    })),
   });
 }
