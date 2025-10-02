a847924985649f43584d0dfb4caf95c3566c9b40
ABUCKY0 <81783950+ABUCKY0@users.noreply.github.com>
2025-10-01 21:54:25 -0500

    feat(sitemap): add sitemap integration and update relevant pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import react from '@astrojs/react'
 import remarkCallout from "@r4ai/remark-callout";
 import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
+import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), icon()],
+  integrations: [react(), icon(), sitemap({
+    filter: (page) => page !== 'https://abucky.dev/test/',
+  }),],
   markdown: {
     shikiConfig: {
       theme: 'dark-plus', // or any Shiki theme name
